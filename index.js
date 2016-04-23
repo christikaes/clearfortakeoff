90e4dbccae5b40bd29a1b778a5b3ff5938bc22de
rsk7 <ravikrishnasingh@gmail.com>
2016-04-23 17:03:13 -0400

    airport mapping

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/*
-Launch web server
-Visit a URL on our server that activates the web scraper
-The scraper will make a request to the website we want to scrape
-The request will capture the HTML of the website and pass it along to our server
-We will traverse the DOM and extract the information we want
-Next, we will format the extracted data into a format we need
-Finally, we will save this formatted data into a JSON file on our machine
-*/
-
 var express = require('express');
 var app = express();
 var path = require("path");
@@ -29,7 +19,6 @@ app.use("/weatherDetails", weatherDetails);
 
 var rtest = require("./routes/rtest");
 app.use("/r", rtest);
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
--- routes/flightdetails.js
+++ routes/flightdetails.js
@@ -1,5 +1,14 @@
 var router = require("express").Router();
 var restler = require("restler");
+var airportdata = require("../airportdata");
+
+/*
+app.get("/airportdata", function(req, res) {
+	var iataindex = 4;
+	var icaoindex = 5;
+	res.send(airportData[0][iataindex] + ", " + airportData[0][icaoindex]);
+});
+*/
 
 // var key_backup = 'christikaes'
 // var secret_backup = 'bf80780c69a92619b60df68ed730e4ba45b01df1'
