5a74973be9e9c55b0b78f6e62ece5d0cf365cc66
rsk7 <ravikrishnasingh@gmail.com>
2016-04-23 12:44:35 -0400

    more endpoints

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-var path    = require("path");
+var path = require("path");
 
+app.use('/static', express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || 3000));
 
 //this is the main page of the app
@@ -9,8 +10,9 @@ app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname + '/views/pages/index.html'));
 });
 
-
-
+app.get('/f/:flightnumber', function(request, response) {
+	response.send("flight number " + request.params.flightnumber);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
