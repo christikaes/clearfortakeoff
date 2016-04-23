4c93d91a55b430a00127bc026a5d89c0d173e366
Christina <christikaes@gmail.com>
2016-04-23 19:07:13 -0400

    update weatherdata to return correct format

--- routes/weatherdata.js
+++ routes/weatherdata.js
@@ -1,20 +1,37 @@
 var router = require("express").Router();
 var restler = require("restler");
+var dateFormat = require('dateformat');
 
-// var username = 'christikaes'
-// var apiKey = 'bf80780c69a92619b60df68ed730e4ba45b01df1'
+var username = 'christikaes'
+var apiKey = 'bf80780c69a92619b60df68ed730e4ba45b01df1'
 
 var fxml_url = "http://flightxml.flightaware.com/json/FlightXML2/";
-var username = "sugaroverflow";
-var apiKey = '321682929bae540c26ce3ff63cd0f1021748db1c';
+// var username = "sugaroverflow";
+// var apiKey = '321682929bae540c26ce3ff63cd0f1021748db1c';
 
 router.get('/:airport', function(req, res) {
 	restler.get(fxml_url + "MetarEx", {
 		username: username,
 		password: apiKey,
-		query: { airport : req.params.airport, howMany: 1 }
+		query: { airport: req.params.airport,
+            howMany: 1 }
 	}).on("success", function(result, response) {
-		res.send(result);
+    var metarData = result.MetarExResult.metar[0];
+    var data = {};
+
+    data.station = req.params.airport;
+    var date = new Date(metarData.time*1000);
+    data.valid = dateFormat(date, "yyyy-dd-mm HH:MM");
+    data.tmpc = metarData.temp_air;
+    data.dwpc = metarData.temp_dewpoint;
+    data.relh = metarData.temp_relhum;
+    data.drct = metarData.wind_direction;
+    data.sknt = metarData.wind_speed;
+    data.alti = metarData.pressure;
+    data.vsby = metarData.visibility;
+    data.gust = metarData.wind_speed_gust;
+
+		res.send(data);
 	});
 });
 
