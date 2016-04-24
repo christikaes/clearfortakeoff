d2ab4b10ab943c5b800f56609efe520cce9afaf8
Christina <christikaes@gmail.com>
2016-04-23 22:55:19 -0400

    styling

---
+++ public/javascript/main.js
@@ -0,0 +1,31 @@
+$(function() {
+
+  var showView = function (page) {
+    $(".contents").hide();
+    $("." + page).show();
+  }
+
+  showView("loading");
+
+  $("#submit").on("click", function(e) {
+      showView("loading");
+
+      e.preventDefault();
+      var flightNumber = $("input[name='flight-number']").val();
+      $.get("/flightdetails/" + flightNumber)
+          .done(function(response) {
+              console.log("success:");
+              console.log(response);
+              showView("result");
+          })
+          .fail(function() {
+              console.log("failed");
+              showView("input");
+              showView("result");
+          });
+    });
+
+    $("#redo").on("click", function(e) {
+        showView("input");
+    });
+});
