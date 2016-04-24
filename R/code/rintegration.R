33d1471ea47c172ef1b2a356c8190c3c6e4cf0ff
rsk7 <ravikrishnasingh@gmail.com>
2016-04-23 21:14:40 -0400

    asdfasdf

--- R/code/rintegration.R
+++ R/code/rintegration.R
@@ -1,2 +1,4 @@
-load("../data/train_rf_noOrigin.RData")
-print(predict(rf.redObj, validate, type = 'prob'))
+library(randomForest)
+load("R/Code/train_rf_noOrigin.RData")
+dat <- predict(rf.predObj, validate, type = 'prob')
+print(dat)
